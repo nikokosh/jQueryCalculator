af4709b85d8bf2358e468754182dc5815aa4706d
nikokosh <annikokosheva@yandex.ru>
2016-08-10 17:54:29 +0200

    js bug fixes and style improvements

--- js/calculator.js
+++ js/calculator.js
@@ -1,129 +1,130 @@
-var numberToShow = "";
+var acceptedLength = 12; // max length that can be displayed on the calculator screen
+var stringToOperate = ''; // string to show
+var tmpNum = ''; // temporary variable to track a decimal. We can adjust as many numbers as we want but only until an operator button is clicked
+var acceptedOperators = ['+', '-', '*', '/', '%'];
+var result = 0; // result of math operations
+var equalClicked = false; // to empty stringToOperate when a number is clicked right after equal button
 
-var numberToOperate;
-
-var operation;
-var result = 0;
-
-var sum = false;
-var subst = false;
-var mult = false;
-var division = false;
-var modulo = false;
-
-
+console.log(equalClicked);
+// NUMBER OR DECIMAL is clicked
 $('.num').click(function() {
-	var oneMoreNum = $(this).text();
-	numberToShow += oneMoreNum;
-	insertToScreen(numberToShow);
+
+    if(equalClicked === false) {
+        if(stringToOperate.length == acceptedLength) {
+            tmpNum = '';
+            stringToOperate = '';
+            insertToScreen('0');
+        }
+        else {
+            var oneMoreNum = $(this).text();
+            // check if clicked number is a valid digit
+            if(isNumeric(parseInt(oneMoreNum))) {
+
+                tmpNum += oneMoreNum;
+                stringToOperate += oneMoreNum;
+                insertToScreen(stringToOperate);
+            }
+        }
+    }
+    else {
+        equalClicked = false;
+        var firstNum = $(this).text();
+        // check if clicked number is a valid digit
+        if(isNumeric(parseInt(firstNum))) {
+
+            tmpNum = firstNum;
+            stringToOperate = firstNum;
+            insertToScreen(stringToOperate);
+        }
+    }
+
 });
 
+
+// OPERATOR is clicked
 $('.operator').click(function() {
-	
 
-	console.log('Result before calcul: ' + result);
-
-	
-
-	if (result == 0) {
-		numberToOperate = parseInt($('#screen').text());
-		result = numberToOperate;
-		operation = $(this).text();
-	}
-	else {
-
-		if(numberToOperate == "afterEqual") {
-			numberToOperate = "";
-			console.log('After equal');
-			operation = $(this).text();
-		}
-		else {
-
-			numberToOperate = parseInt(numberToShow);
-			chooseOperator();
-			calculate(result, numberToOperate);
-			operation = $(this).text();
-		}	
-	}
-
-	console.log('Result is: ' + result);
-	
-	insertToScreen(operation);
-
-	numberToShow = ""; //needed in this positiion
-	
+    console.log(stringToOperate);
+    if(stringToOperate.length < acceptedLength) {
+        // check if previous symbol is already an operator and prevent repeating
+        if(isNumeric(stringToOperate[stringToOperate.length-1])) {
+            // reset tmpNum
+            tmpNum = '';
+            stringToOperate += $(this).text();
+            insertToScreen(stringToOperate);
+        }
+    }
+    equalClicked = false;
 });
 
+// DECIMAL is clicked
+$('#decimal').click(function() {
 
+    if(tmpNum.includes('.') == false) {
+        stringToOperate += '.';
+        tmpNum += '.';
+        insertToScreen(stringToOperate);
+    }
+});
+
+// EQUAL is clicked
 $('#equal').click(function(){
-	console.log('Result before calcul: ' + result);
-	numberToOperate = parseInt(numberToShow); //need to get a fresh number
-	chooseOperator();
-	calculate(result, numberToOperate);
-	insertToScreen(result);
-	numberToOperate = "afterEqual";
-	console.log('Result is: ' + result);
+    //the most important here is
+    //the method eval() that parses a string for me!
+    result = eval(stringToOperate);
+
+    if(result.length <= acceptedLength) {
+        insertToScreen(result);
+        if(result == 0) {
+            stringToOperate = '0';
+        }
+        else {
+            stringToOperate = '' + result;
+        }
+        equalClicked = true;
+    }
+    else {
+        tmpNum = '';
+        stringToOperate = '';
+        insertToScreen('Err');
+    }
 });
 
+
+/*
+AC is clicked
+so we shoul clear the screen and stringToOperate as well
+*/
 $('#clearAll').click(function() {
-	firstNumber = 0;
-	secondNumberToOperate = 0;
 	result = 0;
-	keepInMind = 0;
-	numberToShow = "";
-	insertToScreen(numberToShow);
+	stringToOperate = '';
+	insertToScreen(result);
 });
 
+/*
+CE is clicked
+so we should remove one symbol from stringToOperate
+*/
 $('#clearOne').click(function() {
 	var temporaryStr = $('#screen').text();
 	var stringToInsert = temporaryStr.slice(0, temporaryStr.length-1);
-	console.log(stringToInsert);
-	insertToScreen(stringToInsert);
+    stringToOperate = stringToInsert;
+	insertToScreen(stringToOperate);
 });
 
-function insertToScreen(num) {
-	$('#screen').text(num);
+/**
+ * Function to determine if the last character is an operator or not
+ * @param {Number} num
+ * @return {Bool} true if num isn't digit
+ */
+function isNumeric(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-function calculate(one, two) {
-	if (sum) {
-		result = one + two;
-		sum = false;
-	}
-	else if (subst) {
-		result = one - two;
-		subst = false;
-	}
-	else if (mult) {
-		result = one * two;
-		mult = false;
-	}
-	else if (division) {
-		result = one / two;
-		division = false;
-	}
-	else if (modulo) {
-		result = one % two;
-	}
-}
-
-function chooseOperator() {
-	if (operation == "+") {
-		sum = true;
-	}
-
-	else if (operation == "-") {
-		subst = true;
-	}
-
-	else if (operation == "*") {
-		mult = true;
-	}
-
-	else if (operation == "/") {
-		division = true;
-	}
-	else if (operation == "%") {
-		modulo = true;
-	}
+/**
+ * Appends a string to a certain div
+ * @param {String} data
+ */
+function insertToScreen(data) {
+	$('#screen').text(data);
 }
